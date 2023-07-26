4b575dcb7796c57a6fe7c03f4fa23c6857ed75d4
Zachary Stout <stout.zachary@gmail.com>
2023-07-26 11:18:55 -0600

    removed deno, added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
-import deno from '@astrojs/deno';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations:[ preact()],
-	output: 'server',
-  adapter: deno(),
+  integrations: [preact()],
+  output: 'server',
+  adapter: vercel()
 });
